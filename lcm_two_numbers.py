5e1c65afc038cf2fa64df33db02d569eaf01ea73
aaltopiiri <aaltopiiri@outlook.com>
2020-04-13 17:25:39 +0300

    Edit file

--- lcm_two_numbers.py
+++ lcm_two_numbers.py
@@ -1,5 +1,15 @@
+'''
+In arithmetic and number theory, the least common multiple, lowest common multiple,
+or smallest common multiple of two integers a and b, usually denoted by LCM(a, b),
+is the smallest positive integer that is divisible by both a and b.[1] Since division
+of integers by zero is undefined, this definition has meaning only if a and b are both
+different from zero.
+'''
 count = True
-a, b = 24, 32 #Two numbers
+print("Input first positive integer number from 1 to 100")
+a = int(input()) #First number for example 37
+print("Input second positive integer number from 1 to 100")
+b = int(input()) #Second number for example 53
 i = 1 #Iterator
 while count:
     if (i % a == 0) and (i % b) == 0:
