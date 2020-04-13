a1dc45d4745f930ecbdfa71840deaa7368dfc109
aaltopiiri <aaltopiiri@outlook.com>
2020-04-13 18:38:53 +0300

    Add new file

---
+++ fibonacci_numbers.py
@@ -0,0 +1,15 @@
+'''
+Fibonacci numbers, the elements of the sequence of numbers
+1, 1, 2, 3, 5, 8, 13, 21, …, each of which, after the second,
+is the sum of the two previous numbers.
+'''
+count = True
+a = 0
+#print(a)
+b = 1
+while count:
+    print(b)
+    b, a = a + b, b
+    if b > 1000:
+        break
+a+=1
