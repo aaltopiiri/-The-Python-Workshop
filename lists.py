a5c26d5932c6b488b8441991274fef4073c0d419
aaltopiiri <aaltopiiri@outlook.com>
2020-04-14 09:05:22 +0300

    Add file lists.py

---
+++ lists.py
@@ -0,0 +1,18 @@
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+list3 = list1 + list2
+print(list3)
+len(list3)
+print(list3*3)
+
+shopping = ['milk', 'bread', 'fish', 'banana', 'butter']
+print(shopping)
+shopping[2] = 'cheese'
+print(shopping)
+shopping[0:3]
+shopping[:3]
+shopping[2:3]
+shopping[1:]
+shopping[:-1]
+
+
