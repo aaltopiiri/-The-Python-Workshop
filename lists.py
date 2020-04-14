94c3018fadad5941ddbec91b15dba41683291a8a
aaltopiiri <aaltopiiri@outlook.com>
2020-04-14 17:49:26 +0300

    Add file dictionary.py

--- lists.py
+++ lists.py
@@ -15,4 +15,19 @@ shopping[2:3]
 shopping[1:]
 shopping[:-1]
 
+shopping = ["bread","milk", "eggs"]
+shopping.append("apple")
+print(shopping)
+
+shopping = []
+shopping.append('bread')
+shopping.append('milk')
+shopping.append('eggs')
+shopping.append('apple')
+print(shopping)
+
+shopping.insert(2, 'ham')
+print(shopping)
+
+
 
